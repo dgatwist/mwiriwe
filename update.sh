5c60b7ae0783012a14eac9099141d87fef3e39fd
Andy Nyenimigabo <51054119+dgatwist@users.noreply.github.com>
2022-12-07 08:44:30 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script for update Fsociety tools
 
-git clone --depth=1 https://github.com/Manisso/fsociety.git
+git clone --depth=1 https://github.com/dgatwist/mwiriwe.git
 sudo chmod +x mwiriwe/install.sh
 bash mwiriwe/install.sh
